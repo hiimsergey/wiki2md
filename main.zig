b4af4e7bbab89e6470da81ca349ba7a77874a597
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2025-01-13 12:41:46 +0100

    Fixed fs system

--- main.zig
+++ main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 const Dir = std.fs.Dir;
 const File = std.fs.File;
@@ -7,6 +8,11 @@ const NORMAL: []const u8 = "\x1b[0m";
 const RED: []const u8 = "\x1b[31m";
 const GREEN: []const u8 = "\x1b[32m";
 const CYAN: []const u8 = "\x1b[36m";
+
+const NAME_MAX = switch(builtin.os.tag) {
+    .windows => std.os.windows.NAME_MAX,
+    else => std.os.linux.NAME_MAX
+};
 
 
 /// TODO COMMENT
@@ -21,21 +27,20 @@ fn mkdir(dir: *Dir, dir_basename: []const u8, stderr: anytype) Dir.MakeError!voi
         stderr.print("{s}error{s}: {s}: {s}\n", .{RED, NORMAL, dir_basename, errmsg}) catch {};
         return err;
     };
-    stderr.print("{s}info{s}: {s}: made directory\n", .{GREEN, NORMAL, dir_basename}) catch {};
 }
 
 /// TODO COMMENT
-fn get_output_dir(dir_path: []const u8, stderr: anytype) (Dir.MakeError || Dir.OpenError)!Dir {
-    var cwd = std.fs.cwd();
+fn get_output_dir(dir_path: []const u8, cwd: *Dir, stderr: anytype) (Dir.MakeError || Dir.OpenError)!Dir {
     return cwd.openDir(dir_path, .{}) catch |err| {
         if (err == Dir.OpenError.FileNotFound) {
-            try mkdir(&cwd, dir_path, stderr);
-            return get_output_dir(dir_path, stderr);
+            try mkdir(cwd, dir_path, stderr);
+            return get_output_dir(dir_path, cwd, stderr);
         }
 
         const errmsg = switch (err) {
-            Dir.OpenError.NotDir => "not a directory",
+            Dir.OpenError.AccessDenied => "access denied",
             Dir.OpenError.FileNotFound => "no such directory",
+            Dir.OpenError.NotDir => "not a directory",
             else => "could not open directory"
         };
         stderr.print("{s}error{s}: {s}: {s}\n", .{RED, NORMAL, dir_path, errmsg}) catch {};
@@ -45,24 +50,28 @@ fn get_output_dir(dir_path: []const u8, stderr: anytype) (Dir.MakeError || Dir.O
 
 /// TODO COMMENT
 fn get_output_file(file_basename: []const u8, out_dir: *Dir, stderr: anytype) File.OpenError!File {
-    std.debug.print("TODO: output file: {s}\n", .{file_basename});
-    const result = out_dir.createFile(file_basename, .{}) catch |err| {
+    var out_file_buffer: [NAME_MAX]u8 = undefined;
+    std.mem.copyForwards(u8, &out_file_buffer, file_basename[0..file_basename.len - 4]);
+    std.mem.copyForwards(u8, out_file_buffer[file_basename.len - 4..file_basename.len - 2], "md");
+    const out_file_basename = out_file_buffer[0..file_basename.len - 2];
+
+    const result = out_dir.createFile(out_file_basename, .{}) catch |err| {
         const errmsg = switch (err) {
             File.OpenError.NoSpaceLeft => "no space left in memory",
             File.OpenError.PathAlreadyExists => "already exists",
             else => "could not create file"
         };
-        stderr.print("{s}error{s}: {s}: {s}\n", .{RED, NORMAL, file_basename, errmsg}) catch {};
+        stderr.print("{s}error{s}: {s}: {s}\n", .{RED, NORMAL, out_file_basename, errmsg})
+        catch {};
         return err;
     };
-    stderr.print("{s}info{s}: {s}: created file\n", .{GREEN, NORMAL, file_basename}) catch {};
     return result;
 }
 
 // TODO CONSIDER not using a separate function for that
 /// TODO COMMENT
-fn get_input_file(file_path: []const u8, stderr: anytype) File.OpenError!File {
-    return std.fs.cwd().openFile(file_path, .{}) catch |err| {
+fn get_input_file(file_path: []const u8, cwd: *Dir, stderr: anytype) File.OpenError!File {
+    return cwd.openFile(file_path, .{}) catch |err| {
         stderr.print("{s}error{s}: {s}: could not open", .{RED, NORMAL, file_path}) catch {};
         return err;
     };
@@ -70,21 +79,20 @@ fn get_input_file(file_path: []const u8, stderr: anytype) File.OpenError!File {
 
 /// TODO COMMMENT
 fn transpile_file(file: *File, out_file: *File, stderr: anytype) !void {
-    stderr.print("very well\n", .{}) catch {};
+    // TODO NOW
+    _ = stderr;
     _ = file;
     _ = out_file;
 }
 
-// TODO CONSIDER passings is_absolute here because when recursing to this func for all children
-// of a dir, you can check for is_absolute for one file, since all of them are the same.
+const ReadArgError = Dir.StatFileError || File.OpenError || Dir.MakeError;
+
 /// TODO COMMENT
 /// TODO NOW in order to read dir subargs, i think i need to pass cwd
 /// (keep in mind that closing cwd can lead to a race condition)
 /// so you store the old cwd in main() or something and when recursing advancing
-fn read_arg(path: []const u8, out_dir: *Dir, stderr: anytype)
-(Dir.StatFileError || File.OpenError || Dir.MakeError)!void {
-    std.debug.print("TODO: reading arg {s}\n", .{path});
-    const path_stat: File.Stat = std.fs.cwd().statFile(path) catch |err| {
+fn read_arg(path: []const u8, cwd: *Dir, out_dir: *Dir, stderr: anytype) ReadArgError!void {
+    const path_stat: File.Stat = cwd.statFile(path) catch |err| {
         const errmsg = switch (err) {
             Dir.StatFileError.AccessDenied => "access denied for stat",
             Dir.StatFileError.FileTooBig => "file too large for stat",
@@ -97,7 +105,15 @@ fn read_arg(path: []const u8, out_dir: *Dir, stderr: anytype)
     };
 
     if (path_stat.kind == File.Kind.file) {
-        var file: File = try get_input_file(path, stderr);
+        if (!std.mem.endsWith(u8, path, ".wiki")) {
+            stderr.print("{s}error{s}: {s}: not a vimwiki file\n", .{RED, NORMAL, path}) catch {};
+            // Not an accurate error value but it doesn't matter since I show error through
+            // exit codes.
+            // TODO return File.OpenError.BadPathName;
+            return File.StatError.Unexpected;
+        }
+
+        var file: File = try get_input_file(path, cwd, stderr);
         defer file.close();
 
         const basename = std.fs.path.basename(path);
@@ -120,17 +136,20 @@ fn read_arg(path: []const u8, out_dir: *Dir, stderr: anytype)
         defer out_subdir.close();
 
         // TODO NOW here
-        var dir: Dir = std.fs.cwd().openDir(path, .{ .iterate = true }) catch |err| {
+        var dir: Dir = cwd.openDir(path, .{ .iterate = true }) catch |err| {
             stderr.print(
                 "TODO: {s}error{s}: {s}: could not open directory, but too lazy to write errmsg :/\n",
                 .{RED, NORMAL, path}
             ) catch {};
             return err;
         };
+        defer dir.close();
         var dir_it: Dir.Iterator = dir.iterate();
 
-        std.debug.print("TODO: read_arg: reading children of {s}\n", .{path});
-        while (try dir_it.next()) |entry| try read_arg(entry.name, &out_subdir, stderr);
+        var result: ?ReadArgError = null;
+        while (try dir_it.next()) |entry| read_arg(entry.name, &dir, &out_subdir, stderr)
+        catch |err| { result = err; };
+        return result orelse {};
     }
 }
 
@@ -142,15 +161,16 @@ pub fn main() u8 {
     defer args.deinit();
     _ = args.skip(); // skip "wiki2md"
 
+    var cwd = std.fs.cwd();
+
     // Output directory (first argument)
-    std.debug.print("TODO: main: before making output dir\n", .{});
     var out_dir: Dir = if (args.next()) |arg| blk: {
-        const result = get_output_dir(arg, stderr);
-        std.debug.print("TODO: main: just made output dir, handling left\n", .{});
-        break :blk result catch {
-            std.debug.print("TODO: main: get_output_dir failed :(\n", .{});
-            return 1;
-        };
+        stderr.print(
+            "{s}info{s}: transpiling to {s}{s}{s}\n",
+            .{GREEN, NORMAL, CYAN, arg, NORMAL}
+        ) catch {};
+        const result = get_output_dir(arg, &cwd, stderr);
+        break :blk result catch { return 1; };
     } else {
         stderr.print(
             \\wiki2md – transpile your Vimwiki files to Markdown
@@ -161,18 +181,18 @@ pub fn main() u8 {
         ) catch {};
         return 1;
     };
-    std.debug.print("TODO: main: made output dir:)\n", .{});
     defer out_dir.close();
 
+    var stat: u8 = 0;
+
     // Any input paths (second argument)
-    if (args.next()) |arg| read_arg(arg, &out_dir, stderr) catch { return 1; }
+    if (args.next()) |arg| read_arg(arg, &cwd, &out_dir, stderr) catch { stat = 1; }
     else {
         stderr.print("{s}error{s}: no input files\n", .{RED, NORMAL}) catch {};
         return 1;
     }
 
     // Rest of arguments
-    var stat: u8 = 0;
-    while (args.next()) |arg| read_arg(arg, &out_dir, stderr) catch { stat = 1; };
+    while (args.next()) |arg| read_arg(arg, &cwd, &out_dir, stderr) catch { stat = 1; };
     return stat;
 }
